c4f2ae2b3a4d9b6b5f2486aa2d2850220c06608a
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-09-21 04:53:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,6 +5,12 @@ my_each(collection) do |i|
 end
 
 
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if i < array.length
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
 end
